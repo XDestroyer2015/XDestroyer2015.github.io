e1ea1c230e830c491c015e2b2b391f4dde1d7b1c
XDestroyer2015 <97670862+XDestroyer2015@users.noreply.github.com>
2022-01-19 11:21:56 +0800

    Add files via upload

--- fuctyugdiuadfh.js
+++ fuctyugdiuadfh.js
@@ -1,19 +1,9 @@
-// 顯示讀取遮罩
-function ShowProgressBar() {
-    displayProgress();
-}
-// 隱藏讀取遮罩
-function HideProgressBar() {
-    var progress = $('#divProgress');
-    progress.hide();
-}
-// 顯示讀取畫面
-function displayProgress() {
-    var progress = $('#divProgress');
-    progress.show();
-}
 //取亂數
 function getRandomSentence(x) {
     var index = Math.floor(Math.random() * x);
     return index;
+}
+function over(idname) {
+    //document.getElementById(idname).style.display = 'none';
+    document.getElementById(idname).style.visibility = "hidden";
 }
